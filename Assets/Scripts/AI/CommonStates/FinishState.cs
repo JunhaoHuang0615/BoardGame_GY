51cd284166972b581fc78c25b443aaac8deef828
Huangjunhao <huangjunhao0615@gmail.com>
2025-09-13 17:28:30 +0800

    AI FInish state

---
+++ Assets/Scripts/AI/CommonStates/FinishState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishState : AIStates
+{
+    // Start is called before the first frame update
+    private Unit aiUnit;
+    private FSM fsm;
+
+    public FinishState(FSM fsm)
+    {
+        this.fsm = fsm;
+        this.aiUnit = fsm.aiUnit;
+    }
+
+    public void OnEnter()
+    {
+
+    }
+
+    public void OnExit()
+    {
+
+    }
+
+    public void OnUpdate()
+    {
+
+
+    }
+}
